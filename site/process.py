9c4cecd1d2203b139046fac62d708096c6e06b5a
Adi Eyal <adi@burgercom.co.za>
2013-09-29 10:42:35 +0200

    Added default dummy terminal page

--- site/process.py
+++ site/process.py
@@ -2,6 +2,39 @@ import json
 import os
 from glob import glob
 
+def render_link_page(el):
+    md = """---
+layout: nav
+title: %(title)s
+questions:
+""" % el
+
+    for link in el["links"]:
+        link = str(link)
+        subel = graph[link] 
+        if "page" in subel:
+            href = subel["page"]
+        else:
+            href = "%s.html" % link
+
+        ctx = {
+            "href" : href,
+            "title" : subel["title"],
+            "text" : subel.get("text", "")
+        }
+        md += """ - title: <a href="%(href)s">%(title)s</a>
+   text: "%(text)s"
+""" % ctx
+    md += "---"
+    return md
+
+def render_terminal_page(el):
+    md = """---
+layout: dummy_terminal
+question: %(title)s
+---
+""" % el
+    return md
 
 for html in glob("*.html"):
     os.remove(html)
@@ -12,32 +45,20 @@ stack = ["0"]
 while len(stack) > 0:
     idx = stack.pop()
     el = graph[idx]
+    stack.extend(map(str, el["links"]))
 
     filename = "%s.html" % idx
+    if idx == "0": filename= "index.html"
     if os.path.exists(filename):
         continue
 
-    el["base_page"] = "nav" if el["links"] else "terminal"
-    md = """---
-layout: %(base_page)s
-title: %(title)s
-questions:
-""" % el
+    render_link_page(el)
 
-    for link in el["links"]:
-        link = str(link)
-        stack.append(link)
-        subel = graph[link] 
-        ctx = {
-            "href" : ("%s.html" % link),
-            "title" : subel["title"],
-            "text" : subel.get("text", "")
-        }
-        md += """ - title: <a href="%(href)s">%(title)s</a>
-   text: "%(text)s"
-""" % ctx
+    if el["links"]:
+        md = render_link_page(el)
+    else:
+        md = render_terminal_page(el)
 
-    md += "---"
     fp = open(filename, "w")
     fp.write(md)
     fp.close()
