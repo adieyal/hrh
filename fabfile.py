ef7de06c5f3d1df8486af7bfe9331eeba39e9cb8
Adi Eyal <adi@burgercom.co.za>
2013-10-04 17:09:55 +0200

    Added fabric file for deployment

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+from fabric.api import put, env, local
+from fabric.contrib.project import rsync_project
+
+env.user = 'adi'
+# the servers where the commands are executed
+env.hosts = ['197.221.34.5:2224']
+
+def deploy():
+    local("jekyll build")
+    rsync_project("/var/www/hrh.burgercom.co.za/", "_site/", delete=True)
+
